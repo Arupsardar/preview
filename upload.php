439e8d1ba3d5d50eaf4cbfdf2095e14cc9611db4
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2022-05-11 07:33:16 +0530

    to change

--- upload.php
+++ upload.php
@@ -1,9 +1,26 @@
 <?php
-include 'config.php';
-if($_FILES['image']['name']!=''){
-    $filename =$_FILES['image']['name'];
-    echo $filename;
-}
 
+//upload.php
+
+if(isset($_POST['image']))
+{
+	$data = $_POST['image'];
+
+	
+
+	$image_array_1 = explode(";", $data);
+
+
+	$image_array_2 = explode(",", $image_array_1[1]);
+
+
+	$data = base64_decode($image_array_2[1]);
+
+	$image_name = 'upload/' . time() . '.png';
+
+	file_put_contents($image_name, $data);
+
+	echo $image_name;
+}
 
 ?>
